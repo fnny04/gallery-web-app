94c114cb6e958ea6a3c0e24a59ba2db2b1af1592
Fenny Oktaviani <fnny04@gmail.com>
2023-02-08 23:15:11 +0700

    Update Landing Page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,7 +4,13 @@ const Home = () => {
   return (
     <>
       <div className="home">
-        <p style={{fontSize: '40px'}}>Keep all your memories here and show others</p>
+        <img
+          width={240}
+          src="https://img.icons8.com/clouds/100/null/stack-of-photos.png"
+        />
+        <p style={{ fontSize: "40px" }}>
+          Keep all your memories here and show others
+        </p>
       </div>
     </>
   );
